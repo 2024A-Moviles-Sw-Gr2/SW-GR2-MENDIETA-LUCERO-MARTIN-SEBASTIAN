b4242fd43cf93706cdca4a29f5de967e6961861c
Martín Mendieta <marmenthor@gmail.com>
2024-06-13 08:54:21 -0500

    BListView activity created

--- 01-clases/app/src/main/java/com/example/a2024aswgr2_mm/ACicloVida.kt
+++ 01-clases/app/src/main/java/com/example/a2024aswgr2_mm/ACicloVida.kt
@@ -2,9 +2,12 @@ package com.example.a2024aswgr2_mm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
 import com.google.android.material.snackbar.Snackbar
 
 class ACicloVida : AppCompatActivity() {
+
+    // Se resetea debido a cambios en el HW, como cambiar de Landscape a Portrait
     var textoGlobal = ""
 
     fun mostrarSnackBar(texto: String) {
@@ -50,6 +53,24 @@ class ACicloVida : AppCompatActivity() {
     override fun onDestroy() {
         super.onDestroy()
         mostrarSnackBar("onDestroy")
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        outState.run {
+            // GUARDAR LAS PRIMITIVAS
+            putString("variableTextoGuardado", textoGlobal)
+        }
+        super.onSaveInstanceState(outState)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        // RECUPERAR LA VARIABLE PRIMITIVA
+        val textoRecuperadoDeVariable: String? = savedInstanceState.getString("variableTextoGuardado")
+        if (textoRecuperadoDeVariable != null) {
+            mostrarSnackBar(textoRecuperadoDeVariable)
+            textoGlobal = textoRecuperadoDeVariable
+        }
     }
 
 }
