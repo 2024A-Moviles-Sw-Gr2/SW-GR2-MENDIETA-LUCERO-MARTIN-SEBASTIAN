ae96cb13b48b7adbc0d30d8efbd12b1417b68493
Martín Mendieta <marmenthor@gmail.com>
2024-07-05 09:59:17 -0500

    Intent completo funcionalidad

--- 01-clases/app/src/main/java/com/example/a2024aswgr2_mm/CIntentExplicitoParametros.kt
+++ 01-clases/app/src/main/java/com/example/a2024aswgr2_mm/CIntentExplicitoParametros.kt
@@ -1,11 +1,27 @@
 package com.example.a2024aswgr2_mm
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class CIntentExplicitoParametros : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cintent_explicito_parametros)
+        val nombre = intent.getStringExtra("nombre")
+        val apellido = intent.getStringExtra("apellido")
+        val edad = intent.getIntExtra("edad", 0)
+        val entrenador = intent.getParcelableExtra<BEntrenador>("entrenador")
+        val boton = findViewById<Button>(R.id.btn_devolver_respuesta)
+        boton.setOnClickListener { devolverRespuesta() }
+    }
+
+    fun devolverRespuesta(){
+        val intentDevolverRespuesta = Intent()
+        intentDevolverRespuesta.putExtra("nombreModificado", "Sebastián")
+        // Es posible devolver más parámetros de ser necesario
+        setResult(RESULT_OK, intentDevolverRespuesta)
+        finish()
     }
 }
