4f58f0f6934c234ae4447b13b162088b291356ff
Martín Mendieta <marmenthor@gmail.com>
2024-07-12 10:56:27 -0500

    CRUD with Sqlite logic completed

--- 01-clases/app/src/main/java/com/example/a2024aswgr2_mm/ESqliteHelperEntrenador.kt
+++ 01-clases/app/src/main/java/com/example/a2024aswgr2_mm/ESqliteHelperEntrenador.kt
@@ -101,6 +101,6 @@ class ESqliteHelperEntrenador(
         }
         resultadoConsultaLectura.close()
         baseDatosLectura.close()
-        return arregloRespuesta[0]
+        return if(arregloRespuesta.size > 0) arregloRespuesta[0] else null
     }
 }
--- 01-clases/app/src/main/java/com/example/a2024aswgr2_mm/MainActivity.kt
+++ 01-clases/app/src/main/java/com/example/a2024aswgr2_mm/MainActivity.kt
@@ -56,6 +56,13 @@ class MainActivity : AppCompatActivity() {
             )
             callbackContenidoIntentExplicito.launch(intentExplicito)
         }
+
+        // Inicializar Base de Datos
+        EBaseDeDatos.tablaEntrenador = ESqliteHelperEntrenador(this)
+        val botonSqlite = findViewById<Button>(R.id.btn_sqlite)
+        botonSqlite.setOnClickListener{
+            irActividad(ECrudEntrenador::class.java)
+        }
     }
 
     fun irActividad(
